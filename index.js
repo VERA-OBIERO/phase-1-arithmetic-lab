594a008c2e28f59c42fafd7ee046fe34e5570668
VERA-OBIERO <vera.obiero@gmail.com>
2023-09-27 09:33:04 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*10)+1;
+
+const num3 = 29;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(6,20,9,11,7,18);
